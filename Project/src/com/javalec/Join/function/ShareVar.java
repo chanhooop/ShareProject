21ae9376f623bf1748418a55664a026cb508a5f9
jiyeon29 <cjy3094@naver.com>
2021-04-28 19:12:28 +0900

    join추가

---
+++ Project/src/com/javalec/Join/function/ShareVar.java
@@ -0,0 +1,10 @@
+package com.javalec.Join.function;
+
+public class ShareVar {
+	
+	public static final String url_mysql = "jdbc:mysql://localhost:3316/test1?serverTimezome=UTC&characterEncoding=utf8&useSSL=FALSE";
+	public static final String id_mysql = "root";
+	public static final String pw_mysql = "qwer1234";
+
+	// 여기서는 private가 아니라 불러와야 하므로 "public static"을 써줘야 한다.
+}
