1c746c18b020c7b26d50cab2ef6d4a13cfc435ff
Chanho Lee <cksghwwkd@gmail.com>
2021-04-29 11:09:33 +0900

    aa

--- Project/src/com/javalec/MainPackage/MainProcess.java
+++ Project/src/com/javalec/MainPackage/MainProcess.java
@@ -1,15 +1,15 @@
 package com.javalec.MainPackage;
 
-import com.javalec.Search.Search;
+import com.javalec.Search.Search_CH;
 
 public class MainProcess {
 	
-	Search search;  // 실행시킬 클래스
+	Search_CH search;  // 실행시킬 클래스
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	MainProcess main = new MainProcess();
-	main.search = new Search();
+	main.search = new Search_CH();
 	main.search.setMain(main);
 		
 
