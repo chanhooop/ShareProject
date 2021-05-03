d936b2efbd2706b77e36d57bec69bfb336e5537f
Chanho Lee <cksghwwkd@gmail.com>
2021-05-03 16:25:38 +0900

    찬호끝이요
    ㅁㄴㅇㄹ

--- Project/src/com/javalec/bean/Bean_CH.java
+++ Project/src/com/javalec/bean/Bean_CH.java
@@ -14,7 +14,7 @@ public class Bean_CH {
 	String tfsearch;
 	String conditionQueryColumn;
 	int cmbPriceSelect;
-	
+	int menuCount;
 	
 	//constructor
 	
@@ -41,136 +41,165 @@ public class Bean_CH {
 	}
 
 
+	//---------------------------------------------------------------------------------------------------------	
+		public Bean_CH(String brandName, String menuType, String menuName, String menuprice, int menuCount) {
+			super();
+			this.menuprice = menuprice;
+			this.brandName = brandName;
+			this.menuName = menuName;
+			this.menuType = menuType;
+			this.menuCount = menuCount;
+		}
+
+	//--------------------------------------------------------------
 	
 	// method
 
+		public int getMenuCount() {
+			return menuCount;
+		}
 
 
-	public int getMenuCode() {
-		return menuCode;
+
+
+
+
+
+		public void setMenuCount(int menuCount) {
+			this.menuCount = menuCount;
+		}
+
+
+
+
+
+
+
+		public int getMenuCode() {
+			return menuCode;
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+		public String getMenuType() {
+			return menuType;
+		}
+
+
+
+		public void setMenuType(String menuType) {
+			this.menuType = menuType;
+		}
+
+
+
+		public int getCmbPriceSelect() {
+			return cmbPriceSelect;
+		}
+
+
+
+		public void setCmbPriceSelect(int cmbPriceSelect) {
+			this.cmbPriceSelect = cmbPriceSelect;
+		}
+
+
+
+		public String getConditionQueryColumn() {
+			return conditionQueryColumn;
+		}
+
+
+
+		public void setConditionQueryColumn(String conditionQueryColumn) {
+			this.conditionQueryColumn = conditionQueryColumn;
+		}
+
+
+
+		public String getTfsearch() {
+			return tfsearch;
+		}
+
+
+
+		public void setTfsearch(String tfsearch) {
+			this.tfsearch = tfsearch;
+		}
+
+
+
+		public void setMenuCode(int menuCode) {
+			this.menuCode = menuCode;
+		}
+
+
+
+		public int getBrandCode() {
+			return brandCode;
+		}
+
+
+
+		public void setBrandCode(int brandCode) {
+			this.brandCode = brandCode;
+		}
+
+
+
+		public String getmenuprice() {
+			return menuprice;
+		}
+
+
+
+		public void setmenuprice(String menuprice) {
+			this.menuprice = menuprice;
+		}
+
+
+
+		public String getBrandName() {
+			return brandName;
+		}
+
+
+
+		public void setBrandName(String brandName) {
+			this.brandName = brandName;
+		}
+
+
+
+		public String getMenuName() {
+			return menuName;
+		}
+
+
+
+		public void setMenuName(String menuName) {
+			this.menuName = menuName;
+		}
+
+
+		public String getMetarialName() {
+			return materialAllerge;
+		}
+
+
+		public void setmaterialAllerge(String materialAllerge) {
+			this.materialAllerge = materialAllerge;
+		}
+		
+		
+		
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-	public String getMenuType() {
-		return menuType;
-	}
-
-
-
-	public void setMenuType(String menuType) {
-		this.menuType = menuType;
-	}
-
-
-
-	public int getCmbPriceSelect() {
-		return cmbPriceSelect;
-	}
-
-
-
-	public void setCmbPriceSelect(int cmbPriceSelect) {
-		this.cmbPriceSelect = cmbPriceSelect;
-	}
-
-
-
-	public String getConditionQueryColumn() {
-		return conditionQueryColumn;
-	}
-
-
-
-	public void setConditionQueryColumn(String conditionQueryColumn) {
-		this.conditionQueryColumn = conditionQueryColumn;
-	}
-
-
-
-	public String getTfsearch() {
-		return tfsearch;
-	}
-
-
-
-	public void setTfsearch(String tfsearch) {
-		this.tfsearch = tfsearch;
-	}
-
-
-
-	public void setMenuCode(int menuCode) {
-		this.menuCode = menuCode;
-	}
-
-
-
-	public int getBrandCode() {
-		return brandCode;
-	}
-
-
-
-	public void setBrandCode(int brandCode) {
-		this.brandCode = brandCode;
-	}
-
-
-
-	public String getmenuprice() {
-		return menuprice;
-	}
-
-
-
-	public void setmenuprice(String menuprice) {
-		this.menuprice = menuprice;
-	}
-
-
-
-	public String getBrandName() {
-		return brandName;
-	}
-
-
-
-	public void setBrandName(String brandName) {
-		this.brandName = brandName;
-	}
-
-
-
-	public String getMenuName() {
-		return menuName;
-	}
-
-
-
-	public void setMenuName(String menuName) {
-		this.menuName = menuName;
-	}
-
-
-	public String getMetarialName() {
-		return materialAllerge;
-	}
-
-
-	public void setmaterialAllerge(String materialAllerge) {
-		this.materialAllerge = materialAllerge;
-	}
-	
-	
-	
-}
